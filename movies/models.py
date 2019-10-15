fbf66e2e1dfcd476b4d6f984dd6d2ac83795553c
Michael Frantz <mjfrantz26@gmail.com>
2019-10-15 19:59:14 +0900

    Added Movies Route & DB

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+    field: 
+    Char (string)
+    Int
+    Float
+    Boolean
+"""
+
+class Genre(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=250)
+    release_year = models.IntegerField()
+    in_stock = models.IntegerField()
+    price = models.FloatField()
+    duration_mins = models.IntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name="root"),
+    path('index', views.index, name="index"),
     path('hello', views.index, name="hello"),
     path('welcome', views.welcome, name="welcome"),
     path('about', views.about, name="about")
